49bac8cba296f014fc4fbbfc77359a57fc700289
Aiyara21M <iggamefox@gmail.com>
2024-07-24 08:03:37 +0700

    newdefault

--- BlogProject/blogapp/models.py
+++ BlogProject/blogapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Blog(models.Model):
     title=models.CharField(max_length=50)
     dct=models.CharField(max_length=255)
-    active=models.BooleanField(default=True)
+    active=models.BooleanField(default=False)
